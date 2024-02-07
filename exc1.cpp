872f0a92d1f9ea34e8a4cb01989b2112d4982048
Christina Belikova <119342375+ChristielBel@users.noreply.github.com>
2024-02-07 13:10:50 +0300

    Update exc1.cpp

--- exc1.cpp
+++ exc1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+
+//разобраться и написать коментарии к программе 
 int main()
 {
     int n;
@@ -10,18 +12,22 @@ int main()
     f3 = 0;
     n = n - 2;
     __asm{
-        mov EAX, 1
-        mov EBX, f2
-        mov EDX, 0
-        mov ECX, n
+        mov EAX, 1; копирует / кладет 1 в eax
+        mov EBX, f2; копирует / кладет f2 в ebx
+        mov EDX, 0; копирует / кладет 0 edx
+        ; счетчик
+        mov ECX, n; копирует / кладет n в ecx
 
-        metka : add EDX, EAX
-        add EDX, EBX
-        mov EAX, EBX
-        mov EBX, EDX
-        mov EDX, 0
-        loop metka
-        mov f1, EBX
+        ; add сумма, число.складываает сумму и число, рез - т помещает в сумму
+        ; цикл
+        metka : 
+            add EDX, EAX; к edx прибавляет eax
+            add EDX, EBX; к edx прибавляет ebx
+            mov EAX, EBX; копирует / кладет ebx в eax
+            mov EBX, EDX; копирует / кладет edx в ebx
+            mov EDX, 0; копирует / кладет 0 в edx
+        loop metka  ;уменьшает счетчик
+        mov f1, EBX ; копирует / кладет ebx в f1
     }
     cout << f1;
 }
