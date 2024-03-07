42623b5f43f22a894546b2bf790b0862144c4920
Christina Belikova <119342375+ChristielBel@users.noreply.github.com>
2024-03-07 14:21:46 +0300

    Update scalar.cpp

--- 4/scalar.cpp
+++ 4/scalar.cpp
@@ -3,36 +3,37 @@ using namespace std;
 
 int main()
 {
-    int n = 5;
-    int a[5], b[5];
+    const int n = 3;
+    int a[n], b[n];
+    cout << "Vector A:" << endl;
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
+    cout << "Vector B:" << endl;
     for (int i = 0; i < n; i++){
         cin >> b[i];
     }
     int s = 0;
-    n--;
     __asm {
         lea edx, a
         lea ebx, b
         mov eax, [edx]
-        mul [ebx]
+        imul [ebx]
         mov s, eax
         mov ecx, n
+        cmp ecx, 1
+        je _final
+        dec ecx
         scalar:
             add ebx, 4
             add edx, 4
             mov eax,[edx]
-            mul [ebx]
+            imul [ebx]
             add eax, s
             mov s, eax
         loop scalar
-        mov s,eax
+        _final:
+        mov s, eax
     }
-    cout << s;
+    cout << "Result:" << endl << s;
 }
-
-//x1 y1 z3
-//x2 y2 z2
-//*=x18x2+y18y2+z1*z2
