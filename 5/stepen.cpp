9d3f31cb363fad1137cdec3c14cb4fef5ecd79c8
Christina Belikova <119342375+ChristielBel@users.noreply.github.com>
2024-03-07 14:40:02 +0300

    Update stepen.cpp

--- 5/stepen.cpp
+++ 5/stepen.cpp
@@ -8,18 +8,28 @@ int main()
     __asm {
         mov eax, x
         mov ebx, m
+        cmp ebx, 1
+        je final_loop
+        jl loop_0
         bsr ecx, ebx
         dec ecx
-        _loop:
+        _loop :
         imul eax
             bt ebx, ecx
             jnc loop_u
             imul eax, x
-                loop_u:
-                dec ecx
-                cmp ecx, -1
-                jne _loop
-      mov k, eax
+
+        loop_u :
+            dec ecx
+            cmp ecx, -1
+            jne _loop
+
+        loop_0:
+        cmp eax, x
+        jg final_loop
+        mov eax, 1
+        final_loop:
+        mov k, eax
     }
     cout << k;
 }
